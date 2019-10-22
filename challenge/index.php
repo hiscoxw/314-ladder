7c0679514b575ada6a2bb712f6889a27624abca7
dev <whiscox09@georgefox.edu>
2019-10-22 14:22:43 +0000

    added basic REST skeleton

--- challenge/index.php
+++ challenge/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once("~/public_html/314-ladder/rest.php");
+include_once("../rest.php");
 
 //make an object to process REST requests
 $request = new RestRequest();
@@ -9,7 +9,7 @@ $request = new RestRequest();
 $vars = $request->getRequestVariables();
 
 //connect to the database
-$db = new PDO("pgsql:dbname=teach_yourself host=localhost password=314dev user=dev");
+$db = new PDO("pgsql:dbname=ladder host=localhost password=314dev user=dev");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 function exit_on_failure($test, $msg)
@@ -26,21 +26,29 @@ function exit_on_failure($test, $msg)
 //view
 if($request->isGet())
 {
+   //TODO implement Get
+   $results = array("error_text" => "Get not yet implemented!");
 }
 
 //create
 elseif($request->isPost())
 {
+   //TODO implement Post
+   $results = array("error_text" => "Post not yet implemented!");
 }
 
 //delete
 elseif($request->isDelete())
 {
+   //TODO implement Delete
+   $results = array("error_text" => "Delete not yet implemented!");
 }
 
 //update
 elseif($request->isPut())
 {
+   //TODO implement Put
+   $results = array("error_text" => "Put not yet implemented!");
 }
 
 
