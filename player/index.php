50dfb4ae5b3bda41efb184e2adc99f625cf0383c
dev <whiscox09@georgefox.edu>
2019-10-27 02:26:59 +0000

    ran de-linters on code

--- player/index.php
+++ player/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once("../rest.php");
+require_once "../rest.php";
 
 //make an object to process REST requests
 $request = new RestRequest();
@@ -17,69 +17,66 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 function exit_on_failure($test, $msg)
 {
-   if(!$test)
-   {
-      $results["error_text"] = $msg;
-      echo(json_encode($results));
-      //XXX http_response_code (400);
-      exit();
-   }
+    if(!$test) {
+        $results["error_text"] = $msg;
+        echo(json_encode($results));
+        //XXX http_response_code (400);
+        exit();
+    }
 }
 
 function vars_is_set($variables, $vars)
 {
-   $set = true;
-   foreach($variables as $variable)
-   {
-      if (!isset($vars["$variable"]))
-      {
-         $set = false;
-      }
-   }
+    $set = true;
+    foreach($variables as $variable)
+    {
+        if (!isset($vars["$variable"])) {
+            $set = false;
+        }
+    }
 
-   return $set;
+    return $set;
 }
 
 function execute_sql_query($sql, $args, $db)
 {
-   $statement = $db->prepare($sql);
-   $statement->execute((array)$args);
-   $returnvalue = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $statement = $db->prepare($sql);
+    $statement->execute((array)$args);
+    $returnvalue = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-   return $returnvalue;
+    return $returnvalue;
 }
 
 function player_exists($username, $db)
 {
-   $sql = "SELECT EXISTS(SELECT * FROM player WHERE username = ?);";
-   $exists = execute_sql_query($sql, [$username], $db)[0]["exists"];
+    $sql = "SELECT EXISTS(SELECT * FROM player WHERE username = ?);";
+    $exists = execute_sql_query($sql, [$username], $db)[0]["exists"];
 
-   return $exists == 1;
+    return $exists == 1;
 }
 
 function generate_new_rank($db)
 {
-   $sql = "SELECT MAX(rank) FROM player;";
-   $max = execute_sql_query($sql, [], $db)[0]["max"];
+    $sql = "SELECT MAX(rank) FROM player;";
+    $max = execute_sql_query($sql, [], $db)[0]["max"];
 
-   return $max + 1;
+    return $max + 1;
 }
 
 //view
-if($request->isGet())
-{
+if($request->isGet()) {
    
-   //make sure username is set
-   exit_on_failure(vars_is_set(["username"], $vars), "THE USERNAME IS MISSING!");   
+    //make sure username is set
+    exit_on_failure(vars_is_set(["username"], $vars), "THE USERNAME IS MISSING!");   
 
-   //get the username from vars
-   $username = $vars["username"];
+    //get the username from vars
+    $username = $vars["username"];
 
-   //make sure the player exists
-   exit_on_failure(player_exists($username, $db), "THE REQUESTED PLAYER DOES NOT EXIST!");
+    //make sure the player exists
+    exit_on_failure(player_exists($username, $db), "THE REQUESTED PLAYER DOES NOT EXIST!");
 
-   //create the queries
-   $sql = "SELECT name, username, phone, email, rank,
+    //create the queries
+    $sql = "SELECT name, username, phone, email, rank,
       (
          SELECT CAST(COUNT(m.winner) AS FLOAT) AS wins
             FROM player AS p1 FULL OUTER JOIN match_view AS m ON p1.username = m.winner
@@ -105,59 +102,56 @@ if($request->isGet())
       ) AS game_win_percentage
       FROM player AS p WHERE username = ?;";
       
-   $sql2 = "SELECT AVG(g.winner_score - g.loser_score) AS winning_margin
+    $sql2 = "SELECT AVG(g.winner_score - g.loser_score) AS winning_margin
                FROM player as p, game as g
                WHERE g.winner = ?;";
          
-   $sql3 = "SELECT AVG(g.winner_score - g.loser_score) AS losing_margin
+    $sql3 = "SELECT AVG(g.winner_score - g.loser_score) AS losing_margin
                FROM player AS p, game AS g
                WHERE g.loser = ?
                GROUP BY g.loser;";
 
 
-   //get the results
-   $results = execute_sql_query($sql, [$username], $db)[0];
-   $results["winning_margin"] = execute_sql_query($sql2, [$username], $db)[0]["winning_margin"];
-   $results["losing_margin"] = execute_sql_query($sql3, [$username], $db)[0]["losing_margin"];
+    //get the results
+    $results = execute_sql_query($sql, [$username], $db)[0];
+    $results["winning_margin"] = execute_sql_query($sql2, [$username], $db)[0]["winning_margin"];
+    $results["losing_margin"] = execute_sql_query($sql3, [$username], $db)[0]["losing_margin"];
 
-   //XXX http_response_code (200);
+    //XXX http_response_code (200);
 }
 
 //create
-elseif($request->isPost())
-{
-   //Make sure all the variables are set
-   exit_on_failure(vars_is_set(["name", "email", "phone", "username", "password"], $vars), "ONE OR MORE REQUIRED VARIABLES ARE MISSING!");
+elseif($request->isPost()) {
+    //Make sure all the variables are set
+    exit_on_failure(vars_is_set(["name", "email", "phone", "username", "password"], $vars), "ONE OR MORE REQUIRED VARIABLES ARE MISSING!");
 
-   //Get required variables
-   $name = $vars["name"];
-   $email = $vars["email"];
-   $phone = $vars["phone"];
-   $username = $vars["username"];
-   $password = $vars["password"];
+    //Get required variables
+    $name = $vars["name"];
+    $email = $vars["email"];
+    $phone = $vars["phone"];
+    $username = $vars["username"];
+    $password = $vars["password"];
 
-   //assign a rank
-   $rank = generate_new_rank($db);
+    //assign a rank
+    $rank = generate_new_rank($db);
 
-   //generate and run sql query to add new player
-   $sql = "INSERT INTO player (name, email, phone, username, password, rank) VALUES (?, ?, ?, ?, ?, ?);";
+    //generate and run sql query to add new player
+    $sql = "INSERT INTO player (name, email, phone, username, password, rank) VALUES (?, ?, ?, ?, ?, ?);";
 
-   execute_sql_query($sql, [$name, $email, $phone, $username, $password, $rank], $db);
-   //XXX http_response_code (202);
+    execute_sql_query($sql, [$name, $email, $phone, $username, $password, $rank], $db);
+    //XXX http_response_code (202);
 
-   $results = array("error_text"=>"");
+    $results = array("error_text"=>"");
 }
 
 //delete
-elseif($request->isDelete())
-{
-   //TODO implement Delete
+elseif($request->isDelete()) {
+    //TODO implement Delete
 }
 
 //update
-elseif($request->isPut())
-{
-   //TODO implement Put
+elseif($request->isPut()) {
+    //TODO implement Put
 }
 
 echo(json_encode($results));
